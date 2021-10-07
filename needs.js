da21eb368882aa85e78ab4a58e8281aa4d350e59
anand-bhadauria <90555082+anand-bhadauria@users.noreply.github.com>
2021-10-07 17:24:12 +0530

    Add files via upload

---
+++ needs.js
@@ -0,0 +1,16 @@
+let price='';
+
+function  getData() {
+    price = document.getElementById('submit').value
+    document.cookie = price
+    console.log(price);
+
+    // if(price === '2.4cr')
+    // window.location.href = ''
+
+ }
+
+// function getData() {
+//     console.log(document.cookie);
+// }
+
